43a8ceb5873cea944eafdff06746900d117fb065
Mandeep <meritmandeep35@gmail.com>
2025-03-07 15:59:50 -0800

    Signup database functionality works

---
+++ project360/db_connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = "";  // Empty password since 'root' has no password
+$database = "mv_data"; 
+$port = 3307; // Specify the correct port
+$conn = new mysqli($servername, $username, $password, $database,$port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+
+}
+
+?>
