e7ede4e209ae5b591097ae0e435904508632b4d4
Varun <gargvarun2000@gmail.com>
2025-03-10 21:18:49 -0700

    Login functionality changes

--- header-loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Start session if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Load the correct header dynamically
-include isset($_SESSION["user_id"]) ? "loginheader.php" : "header.php";
-?>
